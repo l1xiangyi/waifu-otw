07559b03665672528b6aa3414b9d4b2ed59ceeaa
Xiangyi Li <venusli370306@gmail.com>
2023-12-16 21:24:41 -0800

    PWA enabled

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { CustomWindow } from './types';
-import Live2D from './Live2D';
-import './Message.ts';
-import NLP from './NLP';
-import SpeechRecognition from './SpeechRecognition';
+import { CustomWindow } from "./types";
+import Live2D from "./Live2D";
+import "./Message.ts";
+import NLP from "./NLP";
+import SpeechRecognition from "./SpeechRecognition";
 
 declare const window: CustomWindow;
 
@@ -12,23 +12,24 @@ window.Modules = {
   recognition: SpeechRecognition,
 };
 
-document.getElementById('loader')!.style.display = 'none';
+document.getElementById("loader")!.style.display = "none";
 
 // TODO: better help pleasze
-document.getElementById('help')!.onclick = () => alert(
-'The laziest type of help you\'ve ever seen.\n' +
-'\n' +
-'> Languages <\n' +
-'Chiai can speak 3 languages! English, Bahasa Indonesia, and Japanese. She ' +
-'can guess what language you\'re using and she\'ll respond in that language.\n' +
-'\n' +
-'> Speech Recognition <\n' +
-'Click the mic button and start speaking! Speech output will be displayed on ' +
-'the message input box and will be sent automatically once you stopped recording.\n' +
-'NOTE: Speech Recognition is not supported on all browsers, so don\'t expect it ' +
-'to work.\n' +
-'\n' +
-'> Anything else <\n' +
-'For more information, you could visit this project\'s repository by clicking ' +
-'the GitHub button, and scroll down.'
-);
+document.getElementById("help")!.onclick = () =>
+  alert(
+    "The laziest type of help you've ever seen.\n" +
+      "\n" +
+      "> Languages <\n" +
+      "Chiai can speak 3 languages! English, Bahasa Indonesia, and Japanese. She " +
+      "can guess what language you're using and she'll respond in that language.\n" +
+      "\n" +
+      "> Speech Recognition <\n" +
+      "Click the mic button and start speaking! Speech output will be displayed on " +
+      "the message input box and will be sent automatically once you stopped recording.\n" +
+      "NOTE: Speech Recognition is not supported on all browsers, so don't expect it " +
+      "to work.\n" +
+      "\n" +
+      "> Anything else <\n" +
+      "For more information, you could visit this project's repository by clicking " +
+      "the GitHub button, and scroll down."
+  );
